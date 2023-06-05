3c005cfec613d21403ae5cadaec759ff2e09ba1d
Lucrombus <Lucrombus@gmail.com>
2023-06-05 17:33:38 +0900

    erd 세부내용 업데이트 및 이력서VO 작업(미완)

---
+++ src/main/java/dev/mvc/resume/ResumeVO.java
@@ -0,0 +1,12 @@
+package dev.mvc.resume;
+
+public class ResumeVO {
+
+//  CREATE TABLE resume(
+//      resumeno                          NUMBER(10)     NOT NULL,
+//      resumep                           VARCHAR2(100)    NULL ,
+//      intro                             VARCHAR2(100)    NULL ,
+//      memberno                          NUMBER(10)     NULL 
+//  );
+  
+}
