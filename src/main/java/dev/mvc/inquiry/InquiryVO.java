6d97ef43d8f56efcb1f4b53aad835f0ca7ab73d2
chaaaaaaaan <shcks23@gmail.com>
2023-06-21 11:24:37 +0900

    문의 VO 제작중

---
+++ src/main/java/dev/mvc/inquiry/InquiryVO.java
@@ -0,0 +1,52 @@
+//  /**********************************/
+//  /* Table Name: 문의 */
+//  /**********************************/
+//  CREATE TABLE inquiry(
+//      inquiryno                          NUMBER(10)     NOT NULL    PRIMARY KEY,
+//      memberno                        INTEGER(10)    NOT NULL,
+//      inquiryReason                    VARCHAR2(100)  NOT NULL ,
+//      contentsno                        NUMBER(10)     NOT NULL ,
+//    FOREIGN KEY (memberno) REFERENCES member (memberno),
+//    FOREIGN KEY (contentsno) REFERENCES contents (contentsno)
+//  );
+
+package dev.mvc.inquiry;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class InquiryVO {
+  
+  /** 문의 번호 */
+  private int inquiryno;
+  /** 회원번호(신고자) */
+  private int memberno;
+  /** 문의 번호 */
+  private String inquiryReason;
+  /** 컨텐츠 번호 */
+  private int contentsno;
+  
+  public int getInquiryno() {
+    return inquiryno;
+  }
+  public void setInquiryno(int inquiryno) {
+    this.inquiryno = inquiryno;
+  }
+  public int getMemberno() {
+    return memberno;
+  }
+  public void setMemberno(int memberno) {
+    this.memberno = memberno;
+  }
+  public String getInquiryReason() {
+    return inquiryReason;
+  }
+  public void setInquiryReason(String inquiryReason) {
+    this.inquiryReason = inquiryReason;
+  }
+  public int getContentsno() {
+    return contentsno;
+  }
+  public void setContentsno(int contentsno) {
+    this.contentsno = contentsno;
+  }
+}
--- src/main/webapp/WEB-INF/doc/first2.ddl
+++ src/main/webapp/WEB-INF/doc/first2.ddl
@@ -473,7 +473,6 @@ CREATE TABLE inquiry(
 		inquiryno                     		NUMBER(10)		 NOT NULL		 PRIMARY KEY,
 		memberno                      		INTEGER(10)		 NOT NULL,
 		inquiryReason                 		VARCHAR2(100)		 NULL ,
-		targetURL                     		VARCHAR2(100)		 NULL ,
 		contentsno                    		NUMBER(10)		 NULL ,
   FOREIGN KEY (memberno) REFERENCES member (memberno),
   FOREIGN KEY (contentsno) REFERENCES contents (contentsno)
@@ -483,7 +482,6 @@ COMMENT ON TABLE inquiry is '문의';
 COMMENT ON COLUMN inquiry.inquiryno is '문의번호';
 COMMENT ON COLUMN inquiry.memberno is '회원번호 (신고자)';
 COMMENT ON COLUMN inquiry.inquiryReason is '문의사유';
-COMMENT ON COLUMN inquiry.targetURL is '대상 URL';
 COMMENT ON COLUMN inquiry.contentsno is '컨텐츠 번호';
 
 
