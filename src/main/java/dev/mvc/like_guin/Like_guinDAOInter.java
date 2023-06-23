49514976b0adb0be206d69d57f00aa725a398d67
p-songsong <dbdnjswls123@naver.com>
2023-06-23 19:38:33 +0900

    관심구인 xml~proc

--- src/main/java/dev/mvc/like_guin/Like_guinDAOInter.java
+++ src/main/java/dev/mvc/like_guin/Like_guinDAOInter.java
@@ -1,12 +1,28 @@
-//package dev.mvc.like_guin;
-//
-//import java.util.ArrayList;
-//
-//public interface Like_guinDAOInter {
-//  
-//  /**
-//   * 관심 구인 목록
-//   * @return
-//   */
-//  public ArrayList<Like_guinVO> list_like_guin(int memberno);
-//}
+package dev.mvc.like_guin;
+
+import java.util.ArrayList;
+
+public interface Like_guinDAOInter {
+    
+  /**
+   * 문의 글 등록
+   * @param inquiryVO
+   * @return
+   */
+  public int create(Like_guinVO like_guinVO);
+  
+  /**
+   * 관심 구인 목록
+   * 
+   * @return
+   */
+  public ArrayList<Like_guinVO> list_like_guin(int memberno);
+  
+  /**
+   * 문의 글 삭제 처리
+   * @param inquiryno
+   * @return
+   */
+  public int delete(int like_guinno);
+  
+}
